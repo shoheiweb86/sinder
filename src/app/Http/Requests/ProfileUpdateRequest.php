534778bed9c6acc199db0c1967bdec6f451f3a55
田辺翔平 <tanashou806@gmail.com>
2023-06-14 11:03:44 +0900

    アイコン、SNS以外のプロフィールを設定できるようにした

--- src/app/Http/Requests/ProfileUpdateRequest.php
+++ src/app/Http/Requests/ProfileUpdateRequest.php
@@ -16,8 +16,18 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['string', 'max:255'],
+            'name' => ['string', 'max:20'],
+            'age' => ['nullable', 'integer'],
+            'grade' => ['nullable', Rule::in(['学部1年生', '学部2年生', '学部3年生', '学部4年生', '学部5年生', '学部6年生', '大学院1年生', '大学院2年生'])],
+            'faculty' => ['nullable', Rule::in(['人文学部', '教育学部', '法学部', '経済科学部', '理学部', '医学部医学科', '医学部保健学科', '歯学部', '工学部', '農学部', '創生学部', '教育実践学研究科', '現代社会文化研究科', '自然科学研究科', '保健学研究科', '医歯学総合研究科'])],
+            'sex' => ['nullable', Rule::in(['男子', '女子', '秘密'])],
+            'self_introduction' => ['nullable', 'string', 'max:200'],
+            'user_icon_path' => ['nullable', 'string'],
+            'line_link' => ['nullable', 'string'],
+            'twitter_link' => ['nullable', 'string'],
+            'instagram_link' => ['nullable', 'string'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            //'password' => ['required', 'string'],
         ];
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 // 新規登録ルート
 Route::get('/register', [RegisteredUserController::class, 'create'])
     ->middleware('guest');
@@ -16,9 +17,11 @@ Route::get('/register', [RegisteredUserController::class, 'create'])
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest');
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
